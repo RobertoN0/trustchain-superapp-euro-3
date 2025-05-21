8c028400ae09c02ab836a756b5b568885a960a6c
Gijs Margadant <g.e.margadant@student.tudelft.nl>
2025-05-21 16:03:45 +0200

    Receiver implemented

---
+++ eurotoken/src/test/java/nl/tudelft/trustchain/eurotoken/offlinePayment/transaction/ReceiverTest.kt
@@ -0,0 +1,8 @@
+package nl.tudelft.trustchain.eurotoken.offlinePayment.transaction
+
+import org.junit.jupiter.api.Assertions.*
+ class ReceiverTest {
+
+@org.junit.jupiter.api.AfterEach
+ fun tearDown() {}
+}
