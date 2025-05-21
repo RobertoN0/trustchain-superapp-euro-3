11f7f09850ecf98c13dfab58055cd61d4aa0c731
Gijs Margadant <g.e.margadant@student.tudelft.nl>
2025-05-21 17:32:21 +0200

    QR code now shows up

--- eurotoken/src/main/java/nl/tudelft/trustchain/eurotoken/ui/offline/OfflineMoneyFragment.kt
+++ eurotoken/src/main/java/nl/tudelft/trustchain/eurotoken/ui/offline/OfflineMoneyFragment.kt
@@ -1,13 +1,22 @@
 package nl.tudelft.trustchain.eurotoken.ui.offline
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import nl.tudelft.trustchain.common.util.viewBinding
 import nl.tudelft.trustchain.eurotoken.R
 import nl.tudelft.trustchain.eurotoken.databinding.FragmentOfflineMoneyBinding
+import nl.tudelft.trustchain.eurotoken.offlinePayment.transaction.BluetoothController
+import nl.tudelft.trustchain.eurotoken.offlinePayment.transaction.Receiver
 import nl.tudelft.trustchain.eurotoken.ui.EurotokenBaseFragment
+import nl.tudelft.trustchain.eurotoken.ui.transfer.RequestMoneyFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +30,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class OfflineMoneyFragment : EurotokenBaseFragment(R.layout.fragment_offline_money) {
     private val binding by viewBinding(FragmentOfflineMoneyBinding::bind)
+    private lateinit var enableBluetoothLauncher: ActivityResultLauncher<Intent>
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,6 +42,16 @@ class OfflineMoneyFragment : EurotokenBaseFragment(R.layout.fragment_offline_mon
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                showToast("Bluetooth enabled")
+            } else {
+                showToast("Bluetooth not enabled")
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,11 +59,33 @@ class OfflineMoneyFragment : EurotokenBaseFragment(R.layout.fragment_offline_mon
 
         binding.receiveMoney.setOnClickListener {
             showToast("Receive clicked")
+            onReceiveClicked()
         }
 
         binding.sendMoney.setOnClickListener {
             showToast("Send clicked")
         }
+    }
+
+    private fun onReceiveClicked() {
+        Log.i("Offline", "Starting bluetooth server")
+        val controller = BluetoothController(
+            context = requireContext(),
+            activityStarter = { intent -> enableBluetoothLauncher.launch(intent) }
+        )
+
+        val receiver = Receiver(controller)
+        val data = receiver.start(getTrustChainCommunity().myPeer, 0) { socket ->
+            showToast("Connection established")
+        }
+
+        Log.i("Offline", "Generating QR: $data")
+        val bundle = Bundle()
+        bundle.putString(RequestMoneyFragment.ARG_DATA, data.toString())
+        findNavController().navigate(
+            R.id.action_offlineMoneyFragment_to_requestMoneyFragment,
+            bundle
+        )
     }
 
     private fun showToast(message: String) {
